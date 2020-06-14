832c6047a144deb5fbbe10a7494b1760feb2652e
Florian Cassayre <florian.cassayre@gmail.com>
2020-06-14 12:18:04 +0200

    Modifications to tableaux.

--- src/main/scala/solver/Tableaux.scala
+++ src/main/scala/solver/Tableaux.scala
@@ -6,6 +6,7 @@ import theory.fol.FOLTheorems._
 import theory.NBGTheory._
 import theory.NBGRules._
 import theory.NBGTheorems._
+import theory.NBGTheory
 
 object Tableaux {
 
@@ -40,8 +41,11 @@ object Tableaux {
             val firstUpdate = equalitiesLeftArg + (x -> (equalitiesLeftArg.getOrElse(x, Set.empty) + t))
             val secondUpdate = firstUpdate + (y -> (firstUpdate.getOrElse(y, Set.empty) + ts))
             copy(map = updatedMap + (ts.formula -> ts), equalitiesLeftArg = secondUpdate)
-          case Member(x, y) => copy(map = updatedMap,
-            membershipsRightArg = membershipsRightArg + (y -> (membershipsRightArg.getOrElse(y, Set.empty) + thm.as[Member[Q, R]]))
+          case Member(x, y) =>
+            val t = thm.as[Member[Q, R]]
+            val ss = isSetIff2(x, y)(t)
+            copy(map = updatedMap + (ss.formula -> ss),
+            membershipsRightArg = membershipsRightArg + (y -> (membershipsRightArg.getOrElse(y, Set.empty) + t))
           )
           case ~(Member(x, y)) => copy(map = updatedMap,
             notMembershipsRightArg = notMembershipsRightArg + (y -> (notMembershipsRightArg.getOrElse(y, Set.empty) + thm.as[~[Member[Q, R]]]))
@@ -71,10 +75,21 @@ object Tableaux {
         }
       }
 
+      def proveIsSet(set: AnySet): Option[Theorem[IsSet[AnySet]]] = set match {
+        case EmptySet => Some(axiomNS.as[IsSet[AnySet]])
+        case PairSet(a, b) => for(sa <- proveIsSet(a); sb <- proveIsSet(b)) yield axiomPS(sa.s, sb.s)(sa)(sb).as[IsSet[AnySet]]
+        case SingletonSet(a) => for(sa <- proveIsSet(a)) yield singletonIsSet(sa).as[IsSet[AnySet]]
+        case OrderedPair(a, b) => for(sa <- proveIsSet(a); sb <- proveIsSet(b)) yield orderedPairSet(sa.s, sb.s)(sa)(sb).as[IsSet[AnySet]]
+        case Intersect(a, b) => proveIsSet(a).map(sa => equalsIsSet(intersectSet(a, b)(sa) #/\ intersectCommutative(a, b)).as[IsSet[AnySet]])
+          .orElse(proveIsSet(b).map(sb => intersectSet(a, b)(sb).as[IsSet[AnySet]]))
+        case Union(a, b) => for(sa <- proveIsSet(a); sb <- proveIsSet(b)) yield unionSet(sa.s, sb.s)(sa)(sb).as[IsSet[AnySet]]
+        case f@SkolemFunction2(a, b) if f.name == valueOf[FA] => Some(isSetFa(a, b).as[IsSet[AnySet]])
+        case other => facts.map.get(IsSet(other)).map(_.as[IsSet[AnySet]])
+      }
+
       thms match {
         case thm +: tail =>
 
-          // TODO: prove oops'
           thm.formula match {
             case atom if facts.map.contains(~atom) => facts.map(~atom).as[~[X]].toImplies(thm)
             case p /\ q => alphaConsequence(thm.as[X /\ Y])
@@ -95,7 +110,11 @@ object Tableaux {
               val t = thm.as[Member[Q, EmptySet]]
               axiomN(z)(t.asSet).toImplies(t)
             case Member(z, SingletonSet(x)) => tableaux(facts, singletonEq(x).toIff(z)(thm.as[Member[Q, SingletonSet[R]]]) +: tail)
-            case Member(z, PairSet(x, y)) => betaBranch(axiomP(x, y, z)(oops(IsSet(x)))(oops(IsSet(y)))(oops(IsSet(z)))(thm.as[Member[Q, PairSet[R, S]]]))
+            case Member(z, PairSet(x, y)) =>
+              val t = thm.as[Member[Q, PairSet[R, S]]]
+              val opt = for(sx <- proveIsSet(x); sy <- proveIsSet(y)) yield betaBranch(axiomP(x, y, z)(sx)(sy)(isSetIff2(z, PairSet(x, y))(t))(t))
+              opt.getOrElse(tableaux(facts.withTheorem(thm), tail))
+            case Member(z, OrderedPair(x, y)) => tableaux(facts, orderedPairEq(x, y).toIff(z)(thm.as[Member[Q, OrderedPair[R, S]]]) +: tail)
             case Member(z, Universe) => tableaux(facts.withTheorem(thm), tail)
             case x === y if !facts.map.contains(thm.formula) && facts.membershipsRightArg.contains(x) =>
               val eq = thm.as[R === S]
@@ -128,11 +147,15 @@ object Tableaux {
                 case Member(z, Difference(x, y)) => betaBranch(notDefinition(differenceContains(x, y, z), thm.as[~[Member[Q, Difference[R, S]]]]))
                 case Member(z, EmptySet) => tableaux(facts.withTheorem(thm), tail)
                 case Member(z, SingletonSet(x)) => tableaux(facts, singletonEq(x).toIff(z).inverse(thm.as[~[Member[Q, SingletonSet[R]]]]) +: tail)
-                case Member(z, PairSet(x, y)) => alphaConsequence(notOr(axiomP(x, y, z)(oops(IsSet(x)))(oops(IsSet(y)))(oops(IsSet(z))).inverse(thm.as[~[Member[Q, PairSet[R, S]]]])))
-                case Member(z, Universe) => tableaux(facts, universeContains(z).inverse(thm.as[~[Member[Q, Universe]]]) +: tail) // TODO?
+                case Member(z, PairSet(x, y)) =>
+                  val t = thm.as[~[Member[Q, PairSet[R, S]]]]
+                  val opt = for(sx <- proveIsSet(x); sy <- proveIsSet(y); sz <- proveIsSet(z)) yield alphaConsequence(axiomP(x, y, z)(sx)(sy)(sz).inverse(t).toAndNot)
+                  opt.getOrElse(tableaux(facts.withTheorem(thm), tail))
+                case Member(z, OrderedPair(x, y)) => tableaux(facts, orderedPairEq(x, y).toIff(z).inverse(thm.as[~[Member[Q, OrderedPair[R, S]]]]) +: tail)
+                case Member(z, Universe) => tableaux(facts, universeContains(z).inverse(thm.as[~[Member[Q, Universe]]]) +: tail)
                 case x === y =>
                   val t = equalsIff2(x, y)
-                  tableaux(facts, equalsIff2(x, y).inverse(thm.as[~[R === S]]) +: tail) // TODO: convert to branch directly
+                  tableaux(facts, equalsIff2(x, y).inverse(thm.as[~[R === S]]) +: tail)
                 case Member(x, y) if !facts.map.contains(thm.formula) && facts.membershipsRightArg.contains(y) =>
                   val ls = facts.membershipsRightArg(y).map(_.as[Member[R, Q]])
                   val ri = thm.as[~[Member[S, Q]]]
@@ -141,18 +164,12 @@ object Tableaux {
                     axiomT(r, s, q).inverse(assume((r in q) <-> (s in q))(h => ri.toImplies(h(l))).toNot)
                   }.toSeq ++ tail)
                 case IsSet(x) =>
-                  val skolemOpt: Option[Theorem[IsSet[_ <: AnySet]]] = x match {
-                    case f@SkolemFunction2(a, b) if f.name == valueOf[FA] => Some(isSetFa(a, b))
-                    // TODO
-                    case _ => None
-                  }
-
-                  skolemOpt.map(skolem => thm.as[~[IsSet[_ <: AnySet]]].toImplies(skolem))
+                  proveIsSet(x).map(skolem => thm.as[~[IsSet[_ <: AnySet]]].toImplies(skolem))
                       .getOrElse(tableaux(facts.withTheorem(thm), tail))
                 case _ => tableaux(facts.withTheorem(thm), tail)
               }
-            // TODO: cut rule
             case _ => tableaux(facts.withTheorem(thm), tail)
+              // Here would go the cut rule
           }
 
         case _ => truth // No more inputs
